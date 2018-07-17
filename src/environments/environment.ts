4a128d3c375d42f9e23d56040c4a843a961d3bb7
Lucia Hill-Rains <luciahillrains@outlook.com>
2018-07-17 14:07:38 -0500

    increase wip num

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
   production: false,
   environment: 'LOCAL',
-  build: 'wip 3',
+  build: 'wip 4',
   newGameStart: "gk_throneroom",
   debugnewGameStart:  "bigRoom"
 };
